713c16e1a2a489b29f063794e4232d87ef97be2c
윤규민 <dbsrbals26@gmail.com>
2022-12-01 22:17:42 +0900

    modify model post,comment

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'users.apps.UsersConfig',
     'board.apps.BoardConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
