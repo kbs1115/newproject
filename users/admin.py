3834be0231ea72274185897b669dd9d4ed7143c5
kbs1115 <bruce11158@gmail.com>
2022-12-01 23:04:50 +0900

    User Customizing and Admin page

---
+++ users/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.utils.translation import gettext_lazy as _
+from .models import User
+
+class MyUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('email', 'userid', 'nickname')
+
+class MyUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'userid', 'nickname')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('userid', 'email', 'nickname', 'password1', 'password2'),
+        }),
+    )
+    form = MyUserChangeForm
+    add_form = MyUserCreationForm
+    list_display = ('email', 'userid', 'nickname', 'date_joined', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('email', 'userid', 'nickname')
+    ordering = ('email', 'date_joined')
+
+admin.site.register(User, MyUserAdmin)
