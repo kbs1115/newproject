24c59499daecbabc9d7f7efb12c83ee82a3acf8a
윤규민 <dbsrbals26@gmail.com>
2022-12-01 11:33:24 +0900

    post,comment model create

--- board/models.py
+++ board/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-123456
+
+# user_id ->user , modifydate추가
+class Post(models.Model):
+    user = models.ManyToManyField(User, on_delete=models.CASCADE, related_name='user_post')
+    subject = models.CharField(max_length=50)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    voter = models.ManyToManyField(User, related_name='voter_post')
+    modify_date = models.DateTimeField()
+
+
+# user_id ->user, voter_id -> voter, post_id->post , modifydate 추가
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField
+    voter = models.ManyToManyField(User, on_delete=models.CASCADE, related_name='voter_comment')
+    user = models.ManyToManyField(User,on_delect=models.CASCADE, related_name='user_comment')
+    modify_date = models.DateTimeField()
+
