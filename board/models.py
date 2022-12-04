fe18403103918d9b06bed400bb1b41a6c04bf20b
윤규민 <dbsrbals26@gmail.com>
2022-12-04 16:03:11 +0900

    add category field in post model

--- board/models.py
+++ board/models.py
@@ -1,26 +1,28 @@
 from django.db import models
 
-# user_id ->user , modifydate추가
+# user_id ->user , modify_date추가
 from users.models import User
 
 
+# 12.4수정) 게시판 종류를 결정하는 카테고리 필드추가
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_post')
     subject = models.CharField(max_length=50)
     content = models.TextField()
     create_date = models.DateTimeField()
     voter = models.ManyToManyField(User, related_name='voter_post')
-    modify_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    category = models.TextField(null=False, blank=False, default="free_board")
 
 
-# user_id ->user, voter_id -> voter, post_id->post , modifydate 추가
+# user_id ->user, voter_id -> voter, post_id->post , modify_date 추가
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField
     voter = models.ManyToManyField(User, related_name='voter_comment')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_comment')
-    modify_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
 
 
 # 파일테이블 추가 Post테이블을 바라봄
