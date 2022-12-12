728bfb350180587f9ea2bbe837fef62294540170
kbs1115 <bruce11158@gmail.com>
2022-12-12 23:18:46 +0900

    user model test passed

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,57 @@
 from django.test import TestCase
+from users.models import User
+from django.utils import timezone
+import datetime
 
-# Create your tests here.
+
+class UserModelTest(TestCase):
+    def test_userCreate(self):# 일반 user model이 잘 의도한 대로 만들어졌는지 테스트한다.
+        user = User.objects.create_user(userid='kbs1115', nickname='byeongsu', email='absoluteqed@gmail.com'
+                                        , password='1234')
+        user.save()
+        time_after_create = timezone.now()
+        self.assertEqual(user.userid, 'kbs1115')
+        self.assertEqual(user.nickname, 'byeongsu')
+        self.assertEqual(user.email, 'absoluteqed@gmail.com')
+        self.assertTrue(user.check_password('1234'))
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertGreater(datetime.timedelta(seconds=1), time_after_create - user.date_joined)
+
+    def test_superuserCreate(self):# superuser model이 잘 의도한 대로 만들어졌는지 테스트한다.
+        user = User.objects.create_superuser(userid='kbs1115', nickname='byeongsu', email='absoluteqed@gmail.com'
+                                        , password='1234')
+        user.save()
+        time_after_create = timezone.now()
+        self.assertEqual(user.userid, 'kbs1115')
+        self.assertEqual(user.nickname, 'byeongsu')
+        self.assertEqual(user.email, 'absoluteqed@gmail.com')
+        self.assertTrue(user.check_password('1234'))
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertGreater(datetime.timedelta(seconds=1), time_after_create - user.date_joined)
+
+    def test_userUpdate(self):# 일반 user model이 잘 의도한 대로 업데이트되었는지 테스트한다.
+        user = User.objects.create_user(userid='kbs1115', nickname='byeongsu', email='absoluteqed@gmail.com'
+                                        , password='1234')
+        user.save()
+        time_after_create = timezone.now()
+        self.assertEqual(user.userid, 'kbs1115')
+        self.assertEqual(user.nickname, 'byeongsu')
+        self.assertEqual(user.email, 'absoluteqed@gmail.com')
+        self.assertTrue(user.check_password('1234'))
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertGreater(datetime.timedelta(seconds=1), time_after_create - user.date_joined)
+
+        user.userid = 'kbskbs'
+        user.nickname = 'KBS'
+        user.email = 'aaaa@naver.com'
+        user.set_password('123456')
+        user.is_staff = True
+
+        self.assertEqual(user.userid, 'kbskbs')
+        self.assertEqual(user.nickname, 'KBS')
+        self.assertEqual(user.email, 'aaaa@naver.com')
+        self.assertTrue(user.check_password('123456'))
+        self.assertTrue(user.is_staff)
