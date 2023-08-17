f350ebdb0317329bcb957bc4315ee21ded39154d
윤규민 <dbsrbals26@gmail.com>
2023-08-17 22:03:29 +0900

    modify lectures views

--- board/views/lectures_views.py
+++ board/views/lectures_views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from board.models import LecturesLink
 
 
+# default 는 category =3, teacher_id 1 -> 영어, 선생님1
+# 번호는 model 참고
 def lectures(request, category: int, teacher_id: int):
-    lecture_obj = LecturesLink.objects.filter(category=category, teacher_id=teacher_id).order_by('-id')
-    context = {'lectures': lecture_obj}
+    lecture_objs = LecturesLink.objects.filter(category=category, teacher=teacher_id).order_by('-id')
+    context = {'lectures': lecture_objs}
     return render(request, 'board/lectures.html', context)
